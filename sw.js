cd3d913cd78a66e2da89551971eeba442c04da6f
Nicolas53200 <nicolas.morel@sdis53.fr>
2026-06-05 14:15:43 +0200

    V.25

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE_NAME = "mes-abeilles-v30";
+const CACHE_NAME = "mes-abeilles-v32";
 self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
