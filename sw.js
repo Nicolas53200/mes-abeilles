8628ec7194db4ba929aa42c6e16f43e74e5d3792
Nicolas53200 <nicolas.morel@sdis53.fr>
2026-06-05 13:08:20 +0200

    V.21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mes-abeilles-v26";
+const CACHE_NAME = "mes-abeilles-v27";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon192.png"];
 
 self.addEventListener("install", event => {
